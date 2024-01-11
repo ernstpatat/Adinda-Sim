4bfe7f77325c8ce9e7cf15559f6712d329936b7b
ernstpatat <desutterernst@gmail.com>
2024-01-11 19:20:50 +0100

    Fixed Firre Bugs

--- Botje.gd
+++ Botje.gd
@@ -1,12 +1,14 @@
 extends Sprite2D
 
-@export var Textures: Array = [preload("res://UniformBotje.png"), preload("res://FakeUniformBotje.png")]
+@export var Textures: Array = [preload("res://Assets/UniformBotje.png"), preload("res://Assets/FakeUniformBotje.png")]
 @export var TextureNr: int = randi() % 2
 @export var BotjePressed: bool = false
+@export var MouseX: int = 0
+@export var MouseY: int = 0
+@export var CheckedOnce: bool = false
 
 func _ready():
 	texture = Textures[TextureNr]
-
 
 func _process(delta):
 	global_position.x = ChangeVar(global_position.x, 315.0, 250.0)
@@ -18,15 +20,22 @@ func ChangeVar(vars, BigNr, SmallNr):
 		FuncNr += ((BigNr-SmallNr)/300)
 	elif FuncNr >= BigNr:
 		FuncNr = SmallNr
+		if texture == Textures[1] and not BotjePressed:
+			if CheckedOnce:
+				print("Hoerenkind ge ebt een zonder uniform laten gaan")
+				CheckedOnce = false
+			else:
+				CheckedOnce = true
 		TextureNr = randi() % 2
 		texture = Textures[TextureNr]
 		BotjePressed = false
 	return FuncNr
 
-func _on_botje_button_pressed():
-	if texture == Textures[0] and not BotjePressed:
-		print("FOUT DOM KUTKIND")
-	elif texture == Textures[1] and not BotjePressed:
-		print("juist + 500 Sociaal Krediet")
-	BotjePressed = true
-	
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if get_viewport_rect().has_point(to_local(event.position)):
+			if texture == Textures[0] and not BotjePressed:
+				print("FOUT DOM KUTKIND")
+			elif texture == Textures[1] and not BotjePressed:
+				print("juist + 500 Sociaal Krediet")
+			BotjePressed = true
--- MeldingsFicheScene.gd
+++ MeldingsFicheScene.gd
@@ -3,10 +3,10 @@ extends Node2D
 func _on_exit_button_pressed():
 	get_tree().change_scene_to_file("res://MonitorScene.tscn")
 
-var database = TextDatabase.new()
-database.load_from_path("res://meldingsfiches.cfg")
+#var database = TextDatabase.new()
+#database.load_from_path("res://meldingsfiches.cfg")
 
-var data = database.get_dictionary()
+#var data = database.get_dictionary()
 
-for item in data:
-	print(item.description)
+#for item in data:
+	#print(item.description)
