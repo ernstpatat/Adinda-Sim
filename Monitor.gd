5390af2321de8c7092da1e7181e4e8268de6f673
ernstpatat <desutterernst@gmail.com>
2024-01-12 20:47:23 +0100

    StartScene+OptionsScene

--- Monitor.gd
+++ Monitor.gd
@@ -13,10 +13,10 @@ func _on_back_button_pressed():
 	ZoomingOut = true
 
 func ZoomOut():
-	global_scale.x = ChangeVar(global_scale.x, 1, 2.148)
-	global_scale.y = ChangeVar(global_scale.y, 1, 2.106)
-	global_position.x = ChangeVar(global_position.x, 158.5, 160)
-	global_position.y = ChangeVar(global_position.y, 85.5, 89.5)
+	global_scale.x = ChangeVar(global_scale.x, 1.0, 2.148)
+	global_scale.y = ChangeVar(global_scale.y, 1.0, 2.106)
+	global_position.x = ChangeVar(global_position.x, 158.5, 160.0)
+	global_position.y = ChangeVar(global_position.y, 85.5, 90.0)
 
 func ChangeVar(vars, BigNr, SmallNr):
 	var FuncNr: float = vars
