3689607490ed2d924fa248460d412fa57a6dc8e3
FastFarre <firredeclippel@gmail.com>
2024-01-15 21:59:20 +0100

    Revert "Merge pull request #1 from ernstpatat/Ernst"
    This reverts commit 05565b511ea2714f2a7289be6a6d7b66d6ab9f1d, reversing
    changes made to 5390af2321de8c7092da1e7181e4e8268de6f673.
    
    reverted main branch (foutje)

--- Monitor.gd
+++ Monitor.gd
@@ -7,7 +7,7 @@ func _process(delta):
 	if ZoomingOut:
 		ZoomOut()
 	if not InMonitor:
-		get_tree().change_scene_to_file("res://MainScene.tscn")
+		get_tree().change_scene_to_file("res://DeskScene.tscn")
 
 func _on_back_button_pressed():
 	ZoomingOut = true
--- StartScene.gd
+++ StartScene.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://MainScene.tscn")
+	get_tree().change_scene_to_file("res://DeskScene.tscn")
 
 func _on_options_button_pressed():
 	get_tree().change_scene_to_file("res://OptionsScene.tscn")
