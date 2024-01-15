24c8c6ee7d474aedc84fc27b2fd41ba9fee50d3e
FastFarre <firredeclippel@gmail.com>
2024-01-15 22:06:22 +0100

    Revert "Merge branch 'meldingsfiche'"
    This reverts commit 244928f9cd20f8648fd6b42f12ec63afbb71a13e, reversing
    changes made to 05565b511ea2714f2a7289be6a6d7b66d6ab9f1d.

--- MeldingsFicheScene.gd
+++ MeldingsFicheScene.gd
@@ -1,25 +1,12 @@
 extends Node2D
 
-@export var meldingsfiches_dict := {}
-var index = 1
-var config = ConfigFile.new()
-var running = true
-
-func _ready():
-	config.load("res://fiches.cfg")
-
 func _on_exit_button_pressed():
 	get_tree().change_scene_to_file("res://MonitorScene.tscn")
 
-func _on_submit_pressed():
-	var config_length = config.get_sections().size()
-	var titel = $Titel.text
-	var naam = $"Naam leerling".text
-	var info = $Beschrijving.text
-	config.set_value(str(config_length), "naam", naam)
-	config.set_value(str(config_length), "titel", titel)
-	config.set_value(str(config_length), "info", info)
-	config.save("res://fiches.cfg")
-	
-func _on_view_meldingsfiches_pressed():
-	get_tree().change_scene_to_file("res://Meldingsfiches_all.tscn")
+#var database = TextDatabase.new()
+#database.load_from_path("res://meldingsfiches.cfg")
+
+#var data = database.get_dictionary()
+
+#for item in data:
+	#print(item.description)
